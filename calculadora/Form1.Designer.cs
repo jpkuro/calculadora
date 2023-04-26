250bbcf290cef30c0814c1b22acefef0e08ba49d
jpkuro <joseluispalmapiguave@gmail.com>
2023-04-25 22:51:43 -0500

    Agregar archivos de proyecto.

---
+++ calculadora/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace calculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cajaresultado = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnuno = new System.Windows.Forms.Button();
+            this.btnnueve = new System.Windows.Forms.Button();
+            this.btnocho = new System.Windows.Forms.Button();
+            this.btnsiete = new System.Windows.Forms.Button();
+            this.btnseis = new System.Windows.Forms.Button();
+            this.btncinco = new System.Windows.Forms.Button();
+            this.btncuatro = new System.Windows.Forms.Button();
+            this.btntres = new System.Windows.Forms.Button();
+            this.btndos = new System.Windows.Forms.Button();
+            this.btnigual = new System.Windows.Forms.Button();
+            this.btnpunto = new System.Windows.Forms.Button();
+            this.btnSuma = new System.Windows.Forms.Button();
+            this.btnresta = new System.Windows.Forms.Button();
+            this.btndivision = new System.Windows.Forms.Button();
+            this.btnmodulo = new System.Windows.Forms.Button();
+            this.btnborrar = new System.Windows.Forms.Button();
+            this.Btncero = new System.Windows.Forms.Button();
+            this.btnlimpiar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cajaresultado
+            // 
+            this.cajaresultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cajaresultado.Location = new System.Drawing.Point(12, 48);
+            this.cajaresultado.Multiline = true;
+            this.cajaresultado.Name = "cajaresultado";
+            this.cajaresultado.ReadOnly = true;
+            this.cajaresultado.Size = new System.Drawing.Size(378, 63);
+            this.cajaresultado.TabIndex = 0;
+            this.cajaresultado.Text = "0";
+            this.cajaresultado.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(150, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Calculadora jose palma";
+            // 
+            // btnuno
+            // 
+            this.btnuno.Font = new System.Drawing.Font("Matura MT Script Capitals", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnuno.Location = new System.Drawing.Point(153, 259);
+            this.btnuno.Name = "btnuno";
+            this.btnuno.Size = new System.Drawing.Size(54, 48);
+            this.btnuno.TabIndex = 2;
+            this.btnuno.Text = "1";
+            this.btnuno.UseVisualStyleBackColor = true;
+            this.btnuno.Click += new System.EventHandler(this.btnuno_Click);
+            // 
+            // btnnueve
+            // 
+            this.btnnueve.Font = new System.Drawing.Font("Matura MT Script Capitals", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnnueve.Location = new System.Drawing.Point(12, 128);
+            this.btnnueve.Name = "btnnueve";
+            this.btnnueve.Size = new System.Drawing.Size(54, 48);
+            this.btnnueve.TabIndex = 3;
+            this.btnnueve.Text = "9";
+            this.btnnueve.UseVisualStyleBackColor = true;
+            this.btnnueve.Click += new System.EventHandler(this.btnnueve_Click);
+            // 
+            // btnocho
+            // 
+            this.btnocho.Font = new System.Drawing.Font("Matura MT Script Capitals", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnocho.Location = new System.Drawing.Point(82, 128);
+            this.btnocho.Name = "btnocho";
+            this.btnocho.Size = new System.Drawing.Size(54, 48);
+            this.btnocho.TabIndex = 4;
+            this.btnocho.Text = "8";
+            this.btnocho.UseVisualStyleBackColor = true;
+            this.btnocho.Click += new System.EventHandler(this.btnocho_Click);
+            // 
+            // btnsiete
+            // 
+            this.btnsiete.Font = new System.Drawing.Font("Matura MT Script Capitals", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsiete.Location = new System.Drawing.Point(153, 128);
+            this.btnsiete.Name = "btnsiete";
+            this.btnsiete.Size = new System.Drawing.Size(54, 48);
+            this.btnsiete.TabIndex = 5;
+            this.btnsiete.Text = "7";
+            this.btnsiete.UseVisualStyleBackColor = true;
+            this.btnsiete.Click += new System.EventHandler(this.btnsiete_Click);
+            // 
+            // btnseis
+            // 
+            this.btnseis.Font = new System.Drawing.Font("Matura MT Script Capitals", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnseis.Location = new System.Drawing.Point(12, 195);
+            this.btnseis.Name = "btnseis";
+            this.btnseis.Size = new System.Drawing.Size(54, 48);
+            this.btnseis.TabIndex = 6;
+            this.btnseis.Text = "6";
+            this.btnseis.UseVisualStyleBackColor = true;
+            this.btnseis.Click += new System.EventHandler(this.btnseis_Click);
+            // 
+            // btncinco
+            // 
+            this.btncinco.Font = new System.Drawing.Font("Matura MT Script Capitals", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncinco.Location = new System.Drawing.Point(82, 195);
+            this.btncinco.Name = "btncinco";
+            this.btncinco.Size = new System.Drawing.Size(54, 48);
+            this.btncinco.TabIndex = 7;
+            this.btncinco.Text = "5";
+            this.btncinco.UseVisualStyleBackColor = true;
+            this.btncinco.Click += new System.EventHandler(this.btncinco_Click);
+            // 
+            // btncuatro
+            // 
+            this.btncuatro.Font = new System.Drawing.Font("Matura MT Script Capitals", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncuatro.Location = new System.Drawing.Point(153, 195);
+            this.btncuatro.Name = "btncuatro";
+            this.btncuatro.Size = new System.Drawing.Size(54, 48);
+            this.btncuatro.TabIndex = 8;
+            this.btncuatro.Text = "4";
+            this.btncuatro.UseVisualStyleBackColor = true;
+            this.btncuatro.Click += new System.EventHandler(this.btncuatro_Click);
+            // 
+            // btntres
+            // 
+            this.btntres.Font = new System.Drawing.Font("Matura MT Script Capitals", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btntres.Location = new System.Drawing.Point(12, 259);
+            this.btntres.Name = "btntres";
+            this.btntres.Size = new System.Drawing.Size(54, 48);
+            this.btntres.TabIndex = 9;
+            this.btntres.Text = "3";
+            this.btntres.UseVisualStyleBackColor = true;
+            this.btntres.Click += new System.EventHandler(this.btntres_Click);
+            // 
+            // btndos
+            // 
+            this.btndos.Font = new System.Drawing.Font("Matura MT Script Capitals", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndos.Location = new System.Drawing.Point(82, 259);
+            this.btndos.Name = "btndos";
+            this.btndos.Size = new System.Drawing.Size(54, 48);
+            this.btndos.TabIndex = 10;
+            this.btndos.Text = "2";
+            this.btndos.UseVisualStyleBackColor = true;
+            this.btndos.Click += new System.EventHandler(this.btndos_Click);
+            // 
+            // btnigual
+            // 
+            this.btnigual.Font = new System.Drawing.Font("Matura MT Script Capitals", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnigual.Location = new System.Drawing.Point(12, 320);
+            this.btnigual.Name = "btnigual";
+            this.btnigual.Size = new System.Drawing.Size(54, 48);
+            this.btnigual.TabIndex = 12;
+            this.btnigual.Text = "=";
+            this.btnigual.UseVisualStyleBackColor = true;
+            // 
+            // btnpunto
+            // 
+            this.btnpunto.Font = new System.Drawing.Font("Matura MT Script Capitals", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnpunto.Location = new System.Drawing.Point(153, 320);
+            this.btnpunto.Name = "btnpunto";
+            this.btnpunto.Size = new System.Drawing.Size(54, 48);
+            this.btnpunto.TabIndex = 13;
+            this.btnpunto.Text = ",";
+            this.btnpunto.UseVisualStyleBackColor = true;
+            // 
+            // btnSuma
+            // 
+            this.btnSuma.Font = new System.Drawing.Font("Matura MT Script Capitals", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSuma.Location = new System.Drawing.Point(227, 128);
+            this.btnSuma.Name = "btnSuma";
+            this.btnSuma.Size = new System.Drawing.Size(54, 48);
+            this.btnSuma.TabIndex = 14;
+            this.btnSuma.Text = "+";
+            this.btnSuma.UseVisualStyleBackColor = true;
+            // 
+            // btnresta
+            // 
+            this.btnresta.Font = new System.Drawing.Font("Matura MT Script Capitals", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnresta.Location = new System.Drawing.Point(227, 259);
+            this.btnresta.Name = "btnresta";
+            this.btnresta.Size = new System.Drawing.Size(54, 48);
+            this.btnresta.TabIndex = 15;
+            this.btnresta.Text = "-";
+            this.btnresta.UseVisualStyleBackColor = true;
+            // 
+            // btndivision
+            // 
+            this.btndivision.Font = new System.Drawing.Font("Matura MT Script Capitals", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndivision.Location = new System.Drawing.Point(227, 195);
+            this.btndivision.Name = "btndivision";
+            this.btndivision.Size = new System.Drawing.Size(54, 48);
+            this.btndivision.TabIndex = 16;
+            this.btndivision.Text = "/";
+            this.btndivision.UseVisualStyleBackColor = true;
+            // 
+            // btnmodulo
+            // 
+            this.btnmodulo.Font = new System.Drawing.Font("Matura MT Script Capitals", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnmodulo.Location = new System.Drawing.Point(227, 320);
+            this.btnmodulo.Name = "btnmodulo";
+            this.btnmodulo.Size = new System.Drawing.Size(54, 48);
+            this.btnmodulo.TabIndex = 17;
+            this.btnmodulo.Text = "%";
+            this.btnmodulo.UseVisualStyleBackColor = true;
+            // 
+            // btnborrar
+            // 
+            this.btnborrar.Font = new System.Drawing.Font("Maiandra GD", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnborrar.Location = new System.Drawing.Point(297, 189);
+            this.btnborrar.Name = "btnborrar";
+            this.btnborrar.Size = new System.Drawing.Size(106, 179);
+            this.btnborrar.TabIndex = 18;
+            this.btnborrar.Text = "Borrar";
+            this.btnborrar.UseVisualStyleBackColor = true;
+            // 
+            // Btncero
+            // 
+            this.Btncero.Font = new System.Drawing.Font("Matura MT Script Capitals", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btncero.Location = new System.Drawing.Point(82, 320);
+            this.Btncero.Name = "Btncero";
+            this.Btncero.Size = new System.Drawing.Size(54, 48);
+            this.Btncero.TabIndex = 19;
+            this.Btncero.Text = "0";
+            this.Btncero.UseVisualStyleBackColor = true;
+            this.Btncero.Click += new System.EventHandler(this.Btncero_Click);
+            // 
+            // btnlimpiar
+            // 
+            this.btnlimpiar.Font = new System.Drawing.Font("Matura MT Script Capitals", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnlimpiar.Location = new System.Drawing.Point(297, 128);
+            this.btnlimpiar.Name = "btnlimpiar";
+            this.btnlimpiar.Size = new System.Drawing.Size(106, 48);
+            this.btnlimpiar.TabIndex = 20;
+            this.btnlimpiar.Text = "C";
+            this.btnlimpiar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 387);
+            this.Controls.Add(this.btnlimpiar);
+            this.Controls.Add(this.Btncero);
+            this.Controls.Add(this.btnborrar);
+            this.Controls.Add(this.btnmodulo);
+            this.Controls.Add(this.btndivision);
+            this.Controls.Add(this.btnresta);
+            this.Controls.Add(this.btnSuma);
+            this.Controls.Add(this.btnpunto);
+            this.Controls.Add(this.btnigual);
+            this.Controls.Add(this.btndos);
+            this.Controls.Add(this.btntres);
+            this.Controls.Add(this.btncuatro);
+            this.Controls.Add(this.btncinco);
+            this.Controls.Add(this.btnseis);
+            this.Controls.Add(this.btnsiete);
+            this.Controls.Add(this.btnocho);
+            this.Controls.Add(this.btnnueve);
+            this.Controls.Add(this.btnuno);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cajaresultado);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox cajaresultado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnuno;
+        private System.Windows.Forms.Button btnnueve;
+        private System.Windows.Forms.Button btnocho;
+        private System.Windows.Forms.Button btnsiete;
+        private System.Windows.Forms.Button btnseis;
+        private System.Windows.Forms.Button btncinco;
+        private System.Windows.Forms.Button btncuatro;
+        private System.Windows.Forms.Button btntres;
+        private System.Windows.Forms.Button btndos;
+        private System.Windows.Forms.Button btnigual;
+        private System.Windows.Forms.Button btnpunto;
+        private System.Windows.Forms.Button btnSuma;
+        private System.Windows.Forms.Button btnresta;
+        private System.Windows.Forms.Button btndivision;
+        private System.Windows.Forms.Button btnmodulo;
+        private System.Windows.Forms.Button btnborrar;
+        private System.Windows.Forms.Button Btncero;
+        private System.Windows.Forms.Button btnlimpiar;
+    }
+}
+
---
+++ calculadora/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculadora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void leerNumero(string numero)
+        {
+            if (cajaresultado.Text == "0" && cajaresultado.Text != null)
+            {
+                cajaresultado.Text = numero;
+            }
+            else
+            {
+                cajaresultado.Text += numero;
+            }
+        }
+
+        private void Btncero_Click(object sender, EventArgs e)
+        {
+            if (cajaresultado.Text == "0")
+            {
+                return;
+            }
+            else
+            {
+                cajaresultado.Text += "0";
+            }
+            
+        }
+
+        private void btnuno_Click(object sender, EventArgs e)
+        {
+            leerNumero("1");
+        }
+
+        private void btndos_Click(object sender, EventArgs e)
+        {
+            leerNumero("2");
+        }
+
+        private void btntres_Click(object sender, EventArgs e)
+        {
+            leerNumero("3");
+        }
+
+        private void btncuatro_Click(object sender, EventArgs e)
+        {
+            leerNumero("4");
+
+        }
+
+        private void btncinco_Click(object sender, EventArgs e)
+        {
+            leerNumero("5");
+        }
+
+        private void btnseis_Click(object sender, EventArgs e)
+        {
+            leerNumero("6");
+        }
+
+        private void btnsiete_Click(object sender, EventArgs e)
+        {
+            leerNumero("7");
+        }
+
+        private void btnocho_Click(object sender, EventArgs e)
+        {
+            leerNumero("8");
+        }
+
+        private void btnnueve_Click(object sender, EventArgs e)
+        {
+            leerNumero("9");
+        }
+    }
+}
